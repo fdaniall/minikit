30ae9852f0dc54fd195d35a1bb64d5bd5880de87
fdaniall <fakhridanial29@gmail.com>
2025-09-28 13:22:32 +0700

    Update minikit configuration and API response structure; change app name, description, and primary category, and remove redirect from next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,6 @@ const nextConfig: NextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
-  async redirects() {
-    return [
-      {
-        source: "/.well-known/farcaster.json",
-        destination:
-          "https://api.farcaster.xyz/miniapps/hosted-manifest/01998e9c-7fbf-484b-3637-16da15f1d0a0",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
